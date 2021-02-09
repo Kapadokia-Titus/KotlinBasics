5d8953281ac91c36e16748a0e34a2432d633b739
Kapadokia-Titus <ktnyandoch@gmail.com>
2021-02-09 09:43:09 +0300

    first commit

---
+++ src/kapadokia/classes/lessons/Condition.kt
@@ -0,0 +1,22 @@
+package kapadokia.classes.lessons
+
+
+
+
+   val  weight  = 83.7
+
+    fun main(args: Array<String>){
+        // the expression
+        if (weight ==50.0){
+            println(" its Tanisha")
+        }
+        else if (weight ==75.0){
+            println(" its Abraham")
+        }
+        else if (weight ==80.0){
+            println(" its Tanisha")
+        }
+        else{
+            println("your age is not accounted for")
+        }
+    }
